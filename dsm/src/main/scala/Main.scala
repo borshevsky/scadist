a3348241d9776c29f302a94c68064114f1a359b6
borshevsky <topicnya@gmail.com>
2020-11-24 11:19:42 +0300

    wip

--- dsm/src/main/scala/Main.scala
+++ dsm/src/main/scala/Main.scala
@@ -7,22 +7,5 @@ import cats.effect.concurrent.Deferred
 import raft._
 
 object Main extends App {
-  def run(args: List[String]): URIO[ZEnv, ExitCode] = {
-    type F[+T] = Task[T]
-
-    val A = Node.make[F]("A")
-    val B = Node.make[F]("B")
-    val C = Node.make[F]("C")
-
-    implicit val cluster = Cluster.make[F](List(A, B, C))
-
-    val CA = Candidate.make[F](A)
-
-    val p = for {
-      d <- Deferred[F, Unit]
-      v <- CA.election(d).exitCode
-    } yield (v)
-
-    p.catchAll(_ => ZIO.succeed(ExitCode.failure))
-  }
+  def run(args: List[String]): URIO[ZEnv, ExitCode] = ???
 }
